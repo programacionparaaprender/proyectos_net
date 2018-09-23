5e2aafddce2ee62b7825d6983a541592335cb11e
alberto13711 <alberto13711@gmail.com>
2018-09-23 11:16:17 -0500

    Consumir un web service en visual basic

---
+++ proyectos_basic/terminal/ConsumirWebService1/ConsumirWebService1/Module1.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Data.SqlClient
+
+Module Module1
+
+    Sub Main()
+        Dim serv1 As New ServicioWeb.DemoSoapClient
+        Console.WriteLine($"Ejemplo de Web Service {serv1.HelloWorld}")
+        Console.ReadKey()
+    End Sub
+
+End Module
