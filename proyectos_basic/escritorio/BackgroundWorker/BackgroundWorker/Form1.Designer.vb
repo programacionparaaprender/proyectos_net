4ce6c02026c337e06fa5dcc7b70d1db2d294752a
alberto13711 <alberto13711@gmail.com>
2019-02-17 17:22:04 -0500

    Ejemplo sencillo de backgroundworker

---
+++ proyectos_basic/escritorio/BackgroundWorker/BackgroundWorker/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnCancelarTarea = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(48, 87)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(97, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Iniciar tarea"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnCancelarTarea
+        '
+        Me.btnCancelarTarea.Location = New System.Drawing.Point(48, 116)
+        Me.btnCancelarTarea.Name = "btnCancelarTarea"
+        Me.btnCancelarTarea.Size = New System.Drawing.Size(97, 23)
+        Me.btnCancelarTarea.TabIndex = 1
+        Me.btnCancelarTarea.Text = "Cancelar tarea"
+        Me.btnCancelarTarea.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(3, 235)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(281, 23)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.btnCancelarTarea)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnCancelarTarea As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ProgressBar1 As ProgressBar
+End Class
---
+++ proyectos_basic/escritorio/BackgroundWorker/BackgroundWorker/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    'https://codigoscript.com/2015/08/21/vb-net-tareas-asincronas-con-backgroundworker/
+
+#Region "INICIO"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackgroundWorker1.WorkerSupportsCancellation = True
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+    End Sub
+#End Region
+
+#Region "EVENTOS"
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        For x As Integer = 0 To 100
+            Threading.Thread.Sleep(100)
+            Me.BackgroundWorker1.ReportProgress(1)
+        Next
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        If (Not Me.ProgressBar1.Value = 100) Then
+            Me.ProgressBar1.Value += e.ProgressPercentage
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        If e.Cancelled Then
+            MessageBox.Show("La operación ha sido cancelada.")
+        ElseIf e.Error IsNot Nothing Then
+            MessageBox.Show("Se ha producido un error durante la ejecución: " & e.Error.Message)
+        Else
+            MessageBox.Show("La operación en segundo plano ha finalizado con éxito.")
+        End If
+    End Sub
+
+    Private Sub btnCancelarTarea_Click(sender As Object, e As EventArgs) Handles btnCancelarTarea.Click
+        If Me.BackgroundWorker1.WorkerSupportsCancellation = True Then
+            Me.btnCancelarTarea.Enabled = False
+            Me.BackgroundWorker1.CancelAsync()
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+#End Region
+
+
+End Class
