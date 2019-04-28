e9cd574064f7d095c3f04cab88bc1bf68a7c71f8
alberto13711 <alberto13711@gmail.com>
2019-04-28 12:38:36 +0200

    Finalización de curso asp.net mvc core 2.0

--- proyectos_c#/web/BookList/BookList/Models/Book.cs
+++ proyectos_c#/web/BookList/BookList/Models/Book.cs
@@ -1,14 +1,17 @@
-/*
-using system;
+
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Task;
- */
+
+ 
 namespace BookList.Models
 {
     public class Book
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
     }
